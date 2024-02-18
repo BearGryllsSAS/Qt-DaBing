95c22de8d8eb1500cd2b5f469b24d84181e35550
haibara ai <2296396559@qq.com>
2024-02-18 21:02:47 +0800

    QMainWindow 工具栏

--- QMainWindowTest/mainwindow.cpp
+++ QMainWindowTest/mainwindow.cpp
@@ -1,6 +1,9 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
 #include <QMessageBox>
+#include <QPushButton>
+#include <QLineEdit>
+#include <QToolBar>
 
 MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent)
@@ -18,6 +21,22 @@ MainWindow::MainWindow(QWidget *parent)
     {
         QMessageBox::information(this, "clicked", "点击保存文件菜单项");
     });
+
+
+
+    // 给工具栏调价按钮和单行输入框
+    ui->toolBar->addWidget(new QPushButton("搜索"));
+    // ui->toolBar->addWidget(new QLineEdit);           // 若想设置输入框的大小则不能传匿名对象
+    QLineEdit* edit = new QLineEdit;
+    // edit->setMaximumWidth(200);                         // 跟随窗口变大，最多200
+    edit->setFixedWidth(100);
+    ui->toolBar->addWidget(edit);
+
+
+
+    // 通过代码添加第二个工具栏对象
+    QToolBar* toolbar = new QToolBar("toolbar");
+    this->addToolBar(Qt::LeftToolBarArea, toolbar);
 }
 
 MainWindow::~MainWindow()
